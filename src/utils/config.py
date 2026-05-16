a5d8c43fc150b9e44a06c1a07e83c912d60f3bf6
Evan Hackstadt <evan.hackstadt@gmail.com>
2026-05-15 23:38:30 -0700

    bug fix config

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,11 +1,23 @@
 # src/utils/config.py
 
+import os
 import yaml
 
-def load_config(path):
-    with open(path, 'r') as f:
+
+# def resolve_path(path_value, script_path):
+#     if os.path.isabs(path_value):
+#         return path_value
+#     return os.path.abspath(os.path.join(script_path, path_value))
+
+def load_config(config_path, script_path):
+    with open(config_path, 'r') as f:
         cfg = yaml.safe_load(f)
 
+    # Resolve relative paths against the experiments script directory
+    # cfg['data']['data_path'] = resolve_path(cfg['data']['data_path'], script_path)
+    # cfg['data']['reactome_path'] = resolve_path(cfg['data']['reactome_path'], script_path)
+    # cfg['logging']['run_dir'] = resolve_path(cfg['logging']['run_dir'], script_path)
+    
     # Validate params
     train = cfg['data']['train']
     val = cfg['data']['val']
